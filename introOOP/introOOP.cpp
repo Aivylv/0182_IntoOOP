ecb6a5b2eb426774b72581695310b584bfb9985b
aivylv <lyvialarasati@gmail.com>
2024-04-23 12:06:46 +0700

    Membuat method output untuk mencetak nilai variabel-variabel data member 'Matakuliah'

--- introOOP/introOOP.cpp
+++ introOOP/introOOP.cpp
@@ -31,6 +31,11 @@ public:
         cin >> sks;
     }
 
+    void output() {
+        cout << "Kode MK: " << kodeMk << endl;
+        cout << "Nama MK: " << namaMk << endl;
+        cout << "SKS: " << sks << endl;
+    }
 };
 
 int main(){
